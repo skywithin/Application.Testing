d99c2636afd0738e323e7a290e61df8b0c91e887
Max Vagner <sydneymax@gmail.com>
2020-06-27 14:48:19 +1000

    Add MockExtensions & JsonMockConverter

---
+++ src/Application.Testing/MockExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace Application.Testing
+{
+    public static class MockExtensions
+    {
+        public static Mock<T> RegisterForJsonSerialization<T>(this Mock<T> mock) where T : class
+        {
+            JsonConvert.DefaultSettings = () => 
+                new JsonSerializerSettings
+                {
+                    Converters = new[] 
+                    { 
+                        new JsonMockConverter() 
+                    }
+                };
+
+            mock.SetupAllProperties();
+
+            JsonMockConverter.RegisterMock(
+                mock,
+                serializer: () => typeof(T).GetProperties().ToDictionary(p => p.Name, p => p.GetValue(mock.Object)));
+
+            return mock;
+        }
+    }
+}
